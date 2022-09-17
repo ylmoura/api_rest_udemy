81706acc39180597146df5949563efbd7d482489
Yves Luan do Rosário Moura <yves.moura@saude.ba.gov.br>
2022-09-16 23:21:57 -0300

    finish filter get hoteis

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, jsonify 
+from flask import Flask, jsonify
 from flask_restful import Api
-from resources.hotel import *
-from resources.usuario import *
+from resources.hotel import Hoteis, Hotel
+from resources.usuario import User, UserRegister, UserLogin, UserLogout
 from flask_jwt_extended import JWTManager
 from blacklist import BLACKLIST
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db' #se precisar mudar o banco de dados só mudar a variavel
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
-app.config['JWT_BLACKLIST_ENABLED'] =True
+app.config['JWT_BLACKLIST_ENABLED'] = True
 api = Api(app)
 jwt = JWTManager(app)
 
@@ -17,22 +18,32 @@ jwt = JWTManager(app)
 @app.before_first_request
 def cria_banco():
     banco.create_all()
+
 @jwt.token_in_blocklist_loader
-def verifica_blacklist(token):
+def verifica_blacklist(self,token):
     return token['jti'] in BLACKLIST
 
 @jwt.revoked_token_loader
-def token_de_acesso_invalidado():
-    return jsonify({'message': 'You have been logged out'}), 401 
+def token_de_acesso_invalidado(self,token):
+            jwtkn = token['jti']
+            return jsonify({'message': 'token {} already been revoked!'.format(jwtkn)}), 401 # unauthorized
 
-api.add_resource(Hoteis, '/hoteis' )
-api.add_resource(Hotel, '/hoteis/<string:hotel_id>' )
-api.add_resource(User, '/usuarios/<int:user_id>' )
-api.add_resource(UserRegister, '/cadastro' )
-api.add_resource(UserLogin, '/login' )
-api.add_resource(UserLogout, '/logout' )
+api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+api.add_resource(User, '/usuarios/<int:user_id>')
+api.add_resource(UserRegister, '/cadastro')
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
 
 if __name__ == '__main__':
     from sql_alchemy import banco
     banco.init_app(app)
     app.run(debug=True)
+
+
+
+#- Substitua @jwt_required por ===> @jwt_required()
+
+#- Renomeie @jwt.token_in_blacklist_loader para ===>  @jwt.token_in_blocklist_loader
+
+#- Substitua get_raw_jwt por ===> get_jwt
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,10 +1,84 @@
+from xml.etree.ElementInclude import LimitedRecursiveIncludeError
 from flask_restful import Resource, reqparse
 from models.hotel import *
 from flask_jwt_extended import jwt_required
+import sqlite3
 
+def normalize_path_params(
+    cidade=None,
+    estrelas_min = 0,
+    estrelas_max = 5,
+    diaria_min = 0,
+    diaria_max = 10000,
+    limite = 50,
+    offset = 0, **dados): #Caso o valor de um parametro seja diferente do default ele irá mudar por causa do **dados
+
+    if cidade:
+        return {
+            'estrelas_min': estrelas_min,
+            'estrelas_max': estrelas_max,
+            'diaria_min': diaria_min,
+            'diaria_max': diaria_max,
+            'cidade': cidade,
+            'limite': limite,
+            'offset':   offset}
+
+    return {'estrelas_min': estrelas_min,
+            'estrelas_max': estrelas_max,
+            'diaria_min': diaria_min,
+            'diaria_max': diaria_max,
+            'limite': limite,
+            'offset': offset}
+
+
+path_params = reqparse.RequestParser()
+path_params.add_argument('cidade', type= str)
+path_params.add_argument('estrelas_min', type= float)
+path_params.add_argument('estrelas_max', type= float)
+path_params.add_argument('diaria_min', type= float)
+path_params.add_argument('diaria_max', type= float)
+path_params.add_argument('limit', type= float)
+path_params.add_argument('offset', type= float)
+
+
+# path /hoteis?cidade= Rio de Janeiro&estrelas_min=4&diaria_max=400
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
+        connection = sqlite3.connect('banco.db')
+        cursor = connection.cursor()
+        dados = path_params.parse_args()
+        # Caso não passe o parametro o dado não será util
+        dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
+        parametros = normalize_path_params(**dados_validos)
+        
+        if not parametros.get('cidade'): # não foi passado cidade no parametros
+            consulta = " SELECT * FROM tb_hoteis\
+            WHERE (estrelas >= ? and estrelas <= ?)\
+            and (diaria >= ? and diaria <= ?)\
+            LIMIT ? OFFSET ?"
+            tupla = tuple([parametros[chave] for chave in parametros]) # valores do where é passado na tuple
+            resultado = cursor.execute(consulta, tupla)
+        else:
+            consulta = "SELECT * FROM tb_hoteis \
+            WHERE (estrelas >= ? and estrelas <= ?) \
+            and (diaria >= ? and diaria <= ?) \
+            and cidade = ? LIMIT ? OFFSET ?"
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta, tupla)
+
+        hoteis = []
+        
+        for linha in resultado:
+            hoteis.append({
+            'hotel_id': linha[0] ,
+            'nome': linha[1],
+            'estrelas': linha[2],
+            'diaria': linha[3],
+            'cidade': linha[4]
+            })
+        
+        return {'hoteis': hoteis} # SELECT * FROM tb_hoteis
+
 
 class Hotel(Resource):
 
